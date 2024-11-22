3e5cb0d0b32a84040daca8d21b9620ab51f8115d
noemie-graignac <noemie.graignac@edu.esiee.fr>
2024-11-22 13:29:55 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 #### Fonction secondaire
-
-
+"""on vérifie si la fonction est un pallindrome"""
 def ispalindrome(p):
-
+    """on regarde si c'est un pallindrome"""
     # votre code ici
-    
-    return False
-
+    a = p.lower()
+    a=a.replace(" ", "")
+    c=str.maketrans({"é": "e","ê": "e","è": "e","ë": "e","à": "a",
+    "?":"","!":"",",": "","'": "",":": "","-": "","ç": "c","ô": "o"})
+    a=a.translate(c)
+    b = a[::-1]
+    return a == b
 #### Fonction principale
-
-
 def main():
-
     # vos appels à la fonction secondaire ici
-
+    """"on vérifie quelque cas pour voir s'il s'agit d'un pallindrome"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
-
 if __name__ == "__main__":
     main()
